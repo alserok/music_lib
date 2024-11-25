1aa377c9c6b51f499f6d56747b489d257e9e2fd8
Alex Rokachev <rokaleser@gmail.com>
2024-11-25 18:26:27 +0300

    added health check route

--- internal/server/http/routes.go
+++ internal/server/http/routes.go
@@ -4,9 +4,15 @@ import (
 	"github.com/alserok/music_lib/internal/server/http/middleware"
 	"github.com/labstack/echo/v4"
 	"github.com/swaggo/echo-swagger"
+	"net/http"
 )
 
 func setupRoutes(s *echo.Echo, h handler) {
+	s.GET("/ping", func(c echo.Context) error {
+		c.JSON(http.StatusOK, "pong")
+		return nil
+	})
+
 	v1 := s.Group("/v1")
 	v1.Use(middleware.WithRecovery(h.log), middleware.WithLogger(h.log), middleware.WithErrorHandler)
 	v1.GET("/swagger/*", echoSwagger.WrapHandler)
